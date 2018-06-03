0b66d011ad355d48faefb25d72e6e6940c53a962
Yordan Danchev <yordangdanchev@gmail.com>
2018-06-03 20:17:50 +0300

    webstorm test

--- react-tuts-playground/src/Person/Person.js
+++ react-tuts-playground/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const person = props => {
-  // your function will recieve one object regardless - object with passed props
+  // your function will receive one object regardless - object with passed props
   return (
     <div>
       <h4>
@@ -16,7 +16,7 @@ const person = props => {
       <hr />
     </div>
   )
-  // props.children reserved world for passing content like slots
-} // we can execute only shor and simple expression in the curly brackets in othe words NO control flow
+  // props.children reserved world for passing content to child component like slots
+} // we can execute only short and simple expression in the curly brackets in other words NO control flow
 
 export default person
