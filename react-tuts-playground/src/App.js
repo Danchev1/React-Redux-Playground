51271ffe96880a8ade28b45948327098216b3c6f
Yordan Danchev <yordangdanchev@gmail.com>
2018-09-19 23:19:23 +0300

    webstorm test

--- react-tuts-playground/src/App.js
+++ react-tuts-playground/src/App.js
@@ -6,26 +6,28 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-        { name: '', age: 1000000 },
-        { name: 'Hooker', age: 17 },
-        { name: 'Nemo', age: 6 }
+        { id: 1, name: '', age: 1000000 },
+        { id: 2, name: 'Hooker', age: 17 },
+        { id: 3, name: 'Nemo', age: 6 }
     ],
-    anotherState: 'unused state'
+    anotherState: 'unused state',
+    showPersons: false
   };
 
-  switchNameHandler = (newValue) => {
-    // console.log(this);
-    // DON'T MUTATE DIRECTLY this.state.persons[0].name = 'Other name'; USE setState from Component library
-    this.setState({
-        persons: [
-            { name: 'MGK', age: 28 },
-            { name: newValue, age: 25 },
-            { name: 'EMINEM', age: 46 }
-        ]
-    })
-  };
+  // switchNameHandler = newValue => {
+  //   // console.log(this);
+  //   // DON'T MUTATE DIRECTLY this.state.persons[0].name = 'Other name'; USE setState from Component library
+  //   this.setState({
+  //       persons: [
+  //           { name: 'MGK', age: 28 },
+  //           { name: newValue, age: 25 },
+  //           { name: 'EMINEM', age: 46 }
+  //       ]
+  //   })
+  // };
 
-  nameChangeHandler = (event) => {
+
+  nameChangeHandler = (event, id) => {
       this.setState({
           persons: [
               { name: 'Mathew', age: 24 },
@@ -35,36 +37,46 @@ class App extends Component {
       })
   };
 
+  deletePersonHandler = (personIndex) => { // always update state in a immutable fashion - copy change set
+    // const persons = this.state.persons;
+    // const persons = this.state.persons.slice(); // slice without args copies the array
+    const persons = [...this.state.persons]; // spreads elements in to the new array - we have new array with elements from old array
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  toggleHandler = () => {
+      const show = this.state.showPersons;
+      this.setState({showPersons: !show});
+  };
+
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+        persons = (
+            <div>
+                {this.state.persons.map((person, index) =>
+                    <Person
+                    name={person.name}
+                    age={person.age}
+                    click={() => this.deletePersonHandler(index)}
+                    changed={(event) => this.nameChangeHandler(event, person.id)}
+                    key={person.id}/>
+                )}
+            </div>
+        );
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <button type="button" onClick={this.switchNameHandler.bind(this, 'Iron Man')}>Switch</button>
+        <button type="button" onClick={this.toggleHandler}>Toggle</button>
         <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
         <hr />
-        <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            click={() => this.switchNameHandler('Rakim')}
-            changed={this.nameChangeHandler}>
-        </Person>
-        <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={() => this.switchNameHandler('Dr. Dre') }
-            changed={this.nameChangeHandler}>
-          <span>My hobbies: the usual</span>
-        </Person>
-        <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={() => this.switchNameHandler('J.Cole') }
-            changed={this.nameChangeHandler}>
-            My hobies: swiming and being stupid
-        </Person>
+        {persons}
       </div>
     )
 
--- react-tuts-playground/src/Person/Person.js
+++ react-tuts-playground/src/Person/Person.js
@@ -8,17 +8,17 @@ const person = props => {
         backgroundColor: '#f8f8f8',
         marginBottom: '20px'
     };
-  return (
+    return (
     <div className="Person" style={personStyle}>
       <h4>This is a person component with some random number: {Math.floor(Math.random() * 30)} <em>(functional component)</em></h4>
       <h1>I am {props.name}, i am {props.age} years old.</h1>
       <hr />
-      <button type="button" onClick={ props.click }>Change name</button>
+      <button type="button" onClick={ props.click }>Delete person</button>
       {props.children ? <p>{props.children}</p> : false}
       <hr />
       <input type="text" onChange={props.changed} value={props.name}/>
     </div>
-  )
+    )
   // props.children reserved world for passing content to child component like slots
 }; // we can execute only short and simple expression in the curly brackets in other words NO control flow
 
