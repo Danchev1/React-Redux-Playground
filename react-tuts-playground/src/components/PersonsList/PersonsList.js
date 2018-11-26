1bf42cf7784076a05c2404bbd811cbb49d51e7b8
yordandanchev <yordangdanchev@gmail.com>
2018-11-26 23:41:06 +0200

    PC t o MAC

--- react-tuts-playground/src/components/PersonsList/PersonsList.js
+++ react-tuts-playground/src/components/PersonsList/PersonsList.js
@@ -2,40 +2,43 @@ import React, { Component } from 'react';
 import Person from "./Person/Person";
 
 class PersonsList extends Component {
-    constructor(props) {
-        super(props);
-        console.log('[PersonsList.js] inside constructor and this is props = ', props)
-    }
+  constructor(props) {
+    super(props);
+    console.log('[PersonsList.js] inside constructor and this is props = ', props)
+  }
 
-    componentWillMount() { console.log('[PersonsList.js] inside componentWillMount()'); }
-    componentDidMount() { console.log('[PersonsList.js] inside componentDidMount()'); }
+  componentWillMount() { console.log('[PersonsList.js] inside componentWillMount()'); }
 
-    componentWillReceiveProps(nextProps) {
-        console.log('[UPDATE PersonsList.js] inside componentWillReceiveProps()', nextProps);
-    }
-    shouldComponentUpdate(nextProps, nextState) {
-      console.log('[UPDATE PersonsList.js] inside shouldComponentUpdate()', nextProps, nextState);
-      return nextProps.persons !== this.props.persons;
-    }
-    componentWillUpdate(nextProps, nextState) {
-      console.log('[UPDATE PersonsList.js] inside componentWillUpdate()', nextProps, nextState);
-    }
+  componentDidMount() { console.log('[PersonsList.js] inside componentDidMount()'); }
 
-    componentDidUpdate() {
-      console.log('[UPDATE PersonsList.js] inside componentDidUpdate()');
-    }
+  componentWillReceiveProps(nextProps) {
+    console.log('[UPDATE PersonsList.js] inside componentWillReceiveProps()', nextProps);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE PersonsList.js] inside shouldComponentUpdate()', nextProps, nextState);
+    return nextProps.persons !== this.props.persons;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE PersonsList.js] inside componentWillUpdate()', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE PersonsList.js] inside componentDidUpdate()');
+  }
 
   render() {
-        console.log('[PersonsList.js] inside render()');
-        return this.props.persons.map((person, index) =>
-            <Person
-                name={person.name}
-                age={person.age}
-                click={() => this.props.deletePersonHandler(index)}
-                changed={(event) => this.props.nameChangeHandler(event, person.id)}
-                key={person.id} />
-        )
-    }
+    console.log('[PersonsList.js] inside render()');
+    return this.props.persons.map((person, index) =>
+        <Person
+            name={person.name}
+            age={person.age}
+            click={() => this.props.deletePersonHandler(index)}
+            changed={(event) => this.props.nameChangeHandler(event, person.id)}
+            key={person.id}/>
+    )
+  }
 }
 
 export default PersonsList;
